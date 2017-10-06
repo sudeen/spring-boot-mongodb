5d89faeab6b11a5189a13e718a10e5e230ae93df
s-kiips <ranjitkarsudeen14@gmail.com>
2017-10-07 00:38:14 +0545

    crud operation added

--- src/main/java/com/sudin/mongodb/springbootmongodb/controllers/HotelController.java
+++ src/main/java/com/sudin/mongodb/springbootmongodb/controllers/HotelController.java
@@ -2,8 +2,7 @@ package com.sudin.mongodb.springbootmongodb.controllers;
 
 import com.sudin.mongodb.springbootmongodb.model.Hotel;
 import com.sudin.mongodb.springbootmongodb.repositories.HotelRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +19,33 @@ public class HotelController {
     @RequestMapping("/all")
     public List<Hotel> getAll(){
         List<Hotel> hotels=this.hotelRepository.findAll();
+        return hotels;
+    }
+
+    @PutMapping
+    public void insert(@RequestBody Hotel hotel){
+        this.hotelRepository.insert(hotel);
+    }
+
+    @PostMapping
+    public void update(@RequestBody Hotel hotel){
+        this.hotelRepository.save(hotel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") String id){
+        this.hotelRepository.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public Hotel getById(@PathVariable("id") String id){
+        Hotel hotel=this.hotelRepository.findById(id);
+        return hotel;
+    }
+
+    @GetMapping("/price/{maxPrice}")
+    public List<Hotel> getByPricePerNight(@PathVariable("maxPrice") int maxPrice){
+        List<Hotel> hotels=this.hotelRepository.findByPricePerNightLessThan(maxPrice);
         return hotels;
     }
 
--- src/main/java/com/sudin/mongodb/springbootmongodb/repositories/HotelRepository.java
+++ src/main/java/com/sudin/mongodb/springbootmongodb/repositories/HotelRepository.java
@@ -4,6 +4,12 @@ import com.sudin.mongodb.springbootmongodb.model.Hotel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HotelRepository extends MongoRepository<Hotel,String> {
+
+    Hotel findById(String id);
+
+    List<Hotel> findByPricePerNightLessThan(int maxPrice);
 }
